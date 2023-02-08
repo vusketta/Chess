f36a4ae51b83d5a0812d4dda528845a00a74dce9
vusketta <vusketta@gmail.com>
2023-02-08 17:30:57 +0300

    Patch 1.1

--- src/main/java/com/gmail/vusketta/HumanPlayer.java
+++ src/main/java/com/gmail/vusketta/HumanPlayer.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class HumanPlayer implements Player {
     private final Map<Character, Integer> NOTATION = Map.of(
-            'a', 1, 'b', 2, 'c', 3, 'd', 4,
-            'e', 5, 'f', 6, 'g', 7, 'h', 8
+            'a', 0, 'b', 1, 'c', 2, 'd', 3,
+            'e', 4, 'f', 5, 'g', 6, 'h', 7
     );
     private final Scanner in;
 
--- src/main/java/com/gmail/vusketta/Main.java
+++ src/main/java/com/gmail/vusketta/Main.java
@@ -4,27 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final Game RANDOM_VS_RANDOM = new TwoPlayerGame(
+        final Game game = new TwoPlayerGame(
                 new ChessBoard(),
-                new RandomPlayer(),
-                new RandomPlayer()
+                getPlayer(),
+                getPlayer()
         );
-        final Game HUMAN_VS_RANDOM = new TwoPlayerGame(
-                new ChessBoard(),
-                new HumanPlayer(new Scanner(System.in)),
-                new RandomPlayer()
-        );
-        final Game HUMAN_VS_HUMAN = new TwoPlayerGame(
-                new ChessBoard(),
-                new HumanPlayer(new Scanner(System.in)),
-                new HumanPlayer(new Scanner(System.in))
-        );
-        final int result = HUMAN_VS_RANDOM.play(true);
+        final int result = game.play(true);
         switch (result) {
             case 1 -> System.out.println("First player won");
             case 2 -> System.out.println("Second player won");
             case 0 -> System.out.println("Draw");
             default -> throw new AssertionError("Unknown result " + result);
+        }
+    }
+
+    private static Player getPlayer() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите \"human\" для обычного игрока или \"random\" для случайного: ");
+        while (true) {
+            switch (in.next()) {
+                case "human" -> {
+                    return new HumanPlayer(new Scanner(System.in));
+                }
+                case "random" -> {
+                    return new RandomPlayer();
+                }
+            }
         }
     }
 }
